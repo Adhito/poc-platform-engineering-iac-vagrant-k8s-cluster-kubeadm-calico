b01d473814143b1107b73e067223d38b3d75f87b
adhito909 <adhito909@gmail.com>
2024-06-26 18:20:37 +0700

    Update setup-master.sh, Stage Save Configs to shared /Vagrant location

--- scripts-setup/setup-master.sh
+++ scripts-setup/setup-master.sh
@@ -3,6 +3,7 @@
 
 
 ## Stage : Setup for Control Plane (Master) servers
+# Variable that needed to be passed $CONTROL_IP, $CONTROL_IP, $POD_CIDR, $SERVICE_CIDR, $NODENAME
 
 set -euxo pipefail
 
@@ -19,3 +20,23 @@ mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+
+
+## Stage : Save Configs to shared /Vagrant location
+# For Vagrant re-runs, check if there is existing configs in the location and delete it for saving new configuration.
+
+config_path="/vagrant/configs"
+
+if [ -d $config_path ]; then
+  rm -f $config_path/*
+else
+  mkdir -p $config_path
+fi
+
+cp -i /etc/kubernetes/admin.conf $config_path/config
+
+touch $config_path/setup-join.sh
+
+chmod +x $config_path/setup-join.sh
+
+kubeadm token create --print-join-command > $config_path/setup-join.sh
