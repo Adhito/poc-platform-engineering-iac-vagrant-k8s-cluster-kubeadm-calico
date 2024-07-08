a2cbe8a799bae5ee643cb8ceedf2c9d6ccf56444
Adhito <adhito909@gmail.com>
2024-07-08 13:41:09 +0700

    Update setup-node-all.sh to reformat stages and add description

--- scripts-setup/setup-node-all.sh
+++ scripts-setup/setup-node-all.sh
@@ -6,7 +6,8 @@
 set -euxo pipefail
 
 
-## Configuration For DNS 
+## Stage : Configuration For DNS 
+# Variable that needed to be passed = ${DNS_SERVERS}
 if [ ! -d /etc/systemd/resolved.conf.d ]; then
 	sudo mkdir /etc/systemd/resolved.conf.d/
 fi
@@ -14,19 +15,19 @@ cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
 [Resolve]
 DNS=${DNS_SERVERS}
 EOF
-
-
 ## Restart network name resolution services (systemd-resolved)
 sudo systemctl restart systemd-resolved
+echo "Stage : Configuration For DNS Completed"
 
 
-## Configuration To Disable Swap & Persist After Reboot 
+## Stage : Configuration To Disable Swap & Persist After Reboot 
 sudo swapoff -a
 (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
 sudo apt-get update -y
+echo "Stage : Configuration To Disable Swap & Persist After Reboot Completed"
 
 
-## Configuration For The .conf File To Load The Modules At Bootup
+## Stage : Configuration For The .conf File To Load The Modules At Bootup
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
@@ -34,21 +35,22 @@ EOF
 
 sudo modprobe overlay
 sudo modprobe br_netfilter
+echo "Stage : Configuration For The .conf File To Load The Modules At Bootup Completed"
 
 
-## Configuration For IP Tables sysctl params Required By Steup & Persist After Reboots
+## Stage : Configuration For IP Tables sysctl params Required By Steup & Persist After Reboots
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
-
-
 ## Restart & Apply sysctl params Without Reboot
 sudo sysctl --system
+echo "Stage : Configuration For IP Tables sysctl params Required By Steup & Persist After Reboots Completed"
 
 
-## Configuration To Install CRI-O Container Runtime
+## Stage : Configuration To Install CRI-O Container Runtime
+# Variable that needed to be passed = ${KUBERNETES_VERSION}
 sudo apt-get update -y
 apt-get install -y software-properties-common curl apt-transport-https ca-certificates
 
@@ -64,8 +66,6 @@ sudo systemctl daemon-reload
 sudo systemctl enable crio --now
 sudo systemctl start crio.service
 
-echo "Stage : CRI-O runtime installed successfully"
-
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -74,3 +74,4 @@ sudo apt-get update -y
 sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
 sudo apt-get update -y
 sudo apt-get install -y jq
+echo "Stage : Configuration To Install CRI-O Container Runtime"
