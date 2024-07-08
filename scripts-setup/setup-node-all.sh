cc99640c6c7d05d828b95398b11fd801250f1c39
Adhito <adhito909@gmail.com>
2024-07-08 13:12:43 +0700

    Create shell script for setup-node-all.sh

---
+++ scripts-setup/setup-node-all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Shell Script Setup for All VM (VM Control Plane and VM Worker Nodes)
+
+
+## Set End the script immediately if any command or pipe exits with a non-zero status.
+set -euxo pipefail
+
+
+## Configuration For DNS 
+if [ ! -d /etc/systemd/resolved.conf.d ]; then
+	sudo mkdir /etc/systemd/resolved.conf.d/
+fi
+cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
+[Resolve]
+DNS=${DNS_SERVERS}
+EOF
+
+
+## Restart network name resolution services (systemd-resolved)
+sudo systemctl restart systemd-resolved
+
+
+# Configuration To Disable Swap
+sudo swapoff -a
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+sudo apt-get update -y
