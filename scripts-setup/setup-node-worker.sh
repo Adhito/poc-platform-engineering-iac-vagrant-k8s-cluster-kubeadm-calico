0931b078b58e211253ed2a41b48699fa5d218f27
Adhito <adhito909@gmail.com>
2024-07-09 22:01:44 +0700

    Create setup-node-worker.sh shell script

---
+++ scripts-setup/setup-node-worker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Shell Script Setup for All Nodes (Control Plane Node and Worker Node)
+
+
+## Set End the script immediately if any command or pipe exits with a non-zero status.
+set -euxo pipefail
