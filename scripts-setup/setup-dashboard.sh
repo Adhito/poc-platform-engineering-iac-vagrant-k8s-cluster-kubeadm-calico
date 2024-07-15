b53c1ae4b1c9a711e85cd0e4689e1e65ed988b1f
Adhito <adhito909@gmail.com>
2024-07-15 16:14:20 +0700

    Update setup-dashboard script to add logic for waiting metric server is ready

--- scripts-setup/setup-dashboard.sh
+++ scripts-setup/setup-dashboard.sh
@@ -4,8 +4,25 @@
 ## Stage : Deploy Kubernetes Dashboard
 # Deploys the Kubernetes dashboard when enabled in settings.yaml
 
+
 ## Set End the script immediately if any command or pipe exits with a non-zero status.
 set -euxo pipefail
 
+
 ## Set Config Path
 config_path="/vagrant/configs"
+
+
+## Stage : Apply Kubernetes Dashboard
+# Logic to wait for the metric server to be ready
+DASHBOARD_VERSION=$(grep -E '^\s*dashboard:' /vagrant/settings.yaml | sed -E -e 's/[^:]+: *//' -e 's/\r$//')
+if [ -n "${DASHBOARD_VERSION}" ]; then
+  while sudo -i -u vagrant kubectl get pods -A -l k8s-app=metrics-server | awk 'split($3, a, "/") && a[1] != a[2] { print $0; }' | grep -v "RESTARTS"; do
+    echo 'Stage : Waiting for metrics server to be ready...'
+    sleep 5
+  done
+  echo 'Stage : Metrics server is ready. Installing dashboard...'
+
+  sudo -i -u vagrant kubectl create namespace kubernetes-dashboard
+
+  echo "Stage : Creating the dashboard user..."
