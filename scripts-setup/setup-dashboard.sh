4d992aa2535f9354d28bf1b32ab6fe21234a774a
Adhito <adhito909@gmail.com>
2024-07-15 16:08:10 +0700

    Create base shell script for kubernetes dashboard

---
+++ scripts-setup/setup-dashboard.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+## Stage : Deploy Kubernetes Dashboard
+# Deploys the Kubernetes dashboard when enabled in settings.yaml
+
+## Set End the script immediately if any command or pipe exits with a non-zero status.
+set -euxo pipefail
