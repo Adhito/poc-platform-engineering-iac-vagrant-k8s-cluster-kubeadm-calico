3ff5009eb7649eb965b5416bececca663a4f5704
Adhito <adhito909@gmail.com>
2025-03-30 20:38:28 +0700

    Update config & cleanup

--- configs/setup-join.sh
+++ configs/setup-join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token rml4ot.vxv0r35gspat3a0y --discovery-token-ca-cert-hash sha256:b39b90dfb04c56bbdd91beba76b6cf3d8f3d9f4b8294d38ea8dbcd8628364a53 
+kubeadm join 192.168.56.10:6443 --token 1qytwo.o34v6j3kopdevnhl --discovery-token-ca-cert-hash sha256:344239dcb559d452d9354159e4fe00693e8432b683554b05a4272dbee994dbef 
